3668b8f4a1f7c834b2db54e16697aaa156b2cf71
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-10-25 11:51:45 +0500

    Routine(package): version increment

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(*paths):
 
 setup(
     name='twitterbot_farm',
-    version='0.0.7',
+    version='0.0.8',
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
     license='GPL',
