128f09775f51125b13c0aec3bfdfeab6d264545e
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-10-26 10:28:50 +0500

    Added(writer): opportunity of change redis host, funciton returns unprocessed tweets

--- twitterbot_farm/writer.py
+++ twitterbot_farm/writer.py
@@ -5,12 +5,16 @@ class Writer(Bot):
 
     """ Reads tweets from reader db. Templating, filter (rate and duplicate) and post. """
 
-    def __init__(self, username,):
-        Bot.__init__(self, username)
+    def __init__(self, username, host='127.0.0.1'):
+        Bot.__init__(self, username, host)
         self.last_tweet_id = int(self.connection.get('__last_tweet_id__', 1))
 
     def tweet(self):
         pass
+
+    def unprocessed_lines(self):
+        return [(key, value) for key, value in self.reader.items() if key.isdigit() and int(key)
+                > int(self.last_tweet_id)]
 
     def is_already_posted(self, words):
         return self.connection.get(words)
