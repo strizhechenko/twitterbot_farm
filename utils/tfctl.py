40a8353648bfd07691d67bedc714b4867e9c4509
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-11-03 19:14:21 +0500

    Added(utils): support of influxdb as storage for metrics from stats
    Usage: INFLUXDB_URL='http://127.0.0.1:8086/write?db=twitterbot_farm' tfctl send_stats

--- utils/tfctl.py
+++ utils/tfctl.py
@@ -6,10 +6,12 @@
 from os import getenv
 from sys import argv
 from json import dumps
+from requests import post
 from dictator import Dictator
 from twitterbot_farm import Reader, Writer
 
 HOST = getenv('HOST', '127.0.0.1')
+INFLUXDB_URL = getenv('INFLUXDB_URL', 'http://127.0.0.1:8086/write?db=twitterbot_farm')
 
 
 def create():
@@ -24,17 +26,29 @@ def dump():
     print dumps(Dictator(host=HOST).items(), indent=4, ensure_ascii=False)
 
 
-def stats():
+def __stats__():
     connect = Dictator(host=HOST)
-    bots = [bot for bot in connect.keys() if bot != '__app__']
+    bots = [bot.replace('@', '') for bot in connect.keys() if bot != '__app__']
+    output = dict()
     for bot_name in bots:
-        role = connect[bot_name][0]
+        role = connect['@' + bot_name][0]
         if role == 'reader':
-            bot = Reader(bot_name.replace('@', ''), HOST)
-            print "{0}={1}".format(bot_name, len(bot.connection.keys()))
+            output[bot_name] = len(Reader(bot_name, HOST).connection.keys())
         elif role == 'writer':
-            bot = Writer(bot_name.replace('@', ''), HOST)
-            print "{0}={1}".format(bot_name, len(bot.unprocessed_lines()))
+            output[bot_name] = len(Writer(bot_name, HOST).unprocessed_lines())
+    return output
+
+
+def stats():
+    for bot_name, value in __stats__().items():
+        print "{0}={1}".format(bot_name, value)
+
+
+def send_stats():
+    for bot_name, value in __stats__().items():
+        data = "{0} value={1}".format(bot_name, value)
+        print INFLUXDB_URL, '-d', data
+        # post(INFLUXDB_URL, data)
 
 
 def main():
@@ -42,6 +56,7 @@ def main():
         'create': create,
         'dump': dump,
         'stats': stats,
+        'send_stats': send_stats,
     }
 
     assert argv[1] in commands, "Unknown command {0}".format(argv[1])
