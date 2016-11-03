7f026567e181dff3f3514e2d5b958c20646cfc57
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-11-03 14:14:18 +0500

    Added(utils): "dump" command, that shows you all data about all bots
    Usage: tfctl dump

--- utils/tfctl.py
+++ utils/tfctl.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python2.7
+#!/usr/local/bin/python2.7
 # coding: utf-8
 
 """ ctl for everything that can't be done without python """
 
 from sys import argv
+from json import dumps
 from dictator import Dictator
-from logging import log, FATAL
+
 
 def create():
     Dictator()[argv[2]] = argv[3:]
@@ -15,11 +16,16 @@ def error():
     raise ValueError(argv)
 
 
+def dump():
+    print dumps(Dictator().items(), indent=4, ensure_ascii=False)
+
+
 def main():
     commands = {
         'create': create,
+        'dump': dump,
     }
-    
+
     assert argv[1] in commands, "Unknown command {0}".format(argv[1])
     commands.get(argv[1])()
 
